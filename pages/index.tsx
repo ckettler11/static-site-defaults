d36ce38844715850dd7738a188a602a2656fcc5c
Casey Kettler <ckettler11@gmail.com>
2022-05-07 11:35:32 -0500

    finish template

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,31 +4,8 @@ import Head from 'next/head'
 const Home: NextPage = () => {
   return <>
     <Head>
-      <title>Home Page Title</title>
+      <title>{process.env.NEXT_PUBLIC_SITE_TITLE}</title>
     </Head>
-
-    <nav className='sticky top-0 py-4 px-8 border-b'>
-      Navigation
-    </nav>
-
-    <main>
-      <header className='p-8 border-b bg-slate-200'> {/* HERO */}
-        Header
-      </header>
-      <div className='flex w-full'>
-        <section className='grow p-8'>
-          Main
-        </section>
-        <aside className='w-72 p-8 border-l'>
-          Aside
-        </aside>
-      </div>
-    </main>
-
-
-    <footer className='border-t p-8'>
-      Footer
-    </footer>
   </>
 }
 
